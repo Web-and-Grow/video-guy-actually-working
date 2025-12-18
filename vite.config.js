6986a021d6ec8aad0829f49461903002afb758f4
Maximilian-Ceolan <maximilian@webandgrow.com>
2025-12-18 17:54:23 +0100

    fix: set base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Video-app/', // Required for GitHub Pages
   plugins: [
     react(),
     VitePWA({
